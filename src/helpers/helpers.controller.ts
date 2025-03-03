f5fa98a47646fdfdd3cd9e6aa90d8af3e9d782ac
AIMtonio <36789102+AIMtonio@users.noreply.github.com>
2025-03-02 20:10:57 -0600

    enable coors

--- src/helpers/helpers.controller.ts
+++ src/helpers/helpers.controller.ts
@@ -7,28 +7,4 @@ import { UpdateHelperDto } from './dto/update-helper.dto';
 export class HelpersController {
   constructor(private readonly helpersService: HelpersService) {}
 
-  @Post()
-  create(@Body() createHelperDto: CreateHelperDto) {
-    return this.helpersService.create(createHelperDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.helpersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.helpersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateHelperDto: UpdateHelperDto) {
-    return this.helpersService.update(+id, updateHelperDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.helpersService.remove(+id);
-  }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Cambia esto según sea necesario
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Admin Dancing API')
     .setDescription('System for admin school dancing')
