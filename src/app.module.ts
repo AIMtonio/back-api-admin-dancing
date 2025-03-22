150d622002de6d383ab546ab5cf6892c6b559b79
AIMtonio <36789102+AIMtonio@users.noreply.github.com>
2025-03-22 10:35:30 -0600

    update entity client

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { UsuarioModule } from './usuario/usuario.module';
-import { AuthModule } from './auth/auth.module';
-import { HelpersModule } from './helpers/helpers.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
   imports: [
@@ -30,6 +29,7 @@ import { AppService } from './app.service';
       signOptions: { expiresIn: '1h' },
     }),
     ConfigModule.forRoot({ isGlobal: true }),
+    ClienteModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/cliente/dto/create-cliente.dto.ts
@@ -0,0 +1,15 @@
+export class CreateClienteDto {
+    id_client: number;
+    name: string;
+    surname?: string;
+    lastname?: string;
+    birthdate: Date;
+    phone: string;
+    email: string;
+    uuid: string;
+    status: number;
+    create_by?: string;
+    create_at?: Date;
+    update_by?: string;
+    update_at?: Date;
+  }
